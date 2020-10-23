5b176dfb2dc8e0e6952d5d5e3fb54f78def3d3a7
Ning <45711839+chty627@users.noreply.github.com>
2020-10-23 19:58:29 +0800

    暂时先做了个简易的首页
    丑陋but还行，之后慢慢改样子

---
+++ Danbo/frontend/src/views/Home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+      <v-card class="overflow-hidden" yellow lighten-5>
+        <TopBar />
+        <Tweets />
+      </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DashboardIndex",
+
+  components: {
+    TopBar: () => import("../components/TopBar"),
+    Tweets: () => import("../components/Tweets"),
+    // DashboardCoreDrawer: () => import('./components/Drawer'),
+    // DashboardCoreSettings: () => import('./components/Settings'),
+    // DashboardCoreView: () => import('./components/View'),
+  },
+
+  data: () => ({
+    list: [1, 2, 3, 4, 5, 6],
+  }),
+};
+</script>
---
+++ Danbo/frontend/src/views/components/Tweets.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div v-for="i in list" :key="i">
+      <v-row justify="center">
+        <v-card class="mx-auto" outlined color="#Faaaaa" dark max-width="1200" >
+          <v-card-title>
+            <v-list-item-avatar color="grey darken-3">
+                <v-img
+                  class="elevation-6"
+                  alt=""
+                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                ></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>Evan You</v-list-item-title>
+              </v-list-item-content>
+          </v-card-title>
+
+          <v-card-text class="headline font-weight-bold">
+            Turns out semicolon-less style is easier and safer in TS because
+            most gotcha edge cases are type invalid as well.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-list-item class="grow">
+              <v-list-item-avatar color="grey darken-3">
+                <v-img
+                  class="elevation-6"
+                  alt=""
+                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                ></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>Evan You</v-list-item-title>
+              </v-list-item-content>
+
+              <v-row align="center" justify="end">
+                <v-icon class="mr-1">
+                  mdi-heart
+                </v-icon>
+                <span class="subheading mr-2">256</span>
+                <span class="mr-1">·</span>
+                <v-icon class="mr-1">
+                  mdi-share-variant
+                </v-icon>
+                <span class="subheading">45</span>
+              </v-row>
+            </v-list-item>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tweets",
+
+  components: {},
+
+  data: () => ({
+    list: [1, 2, 3, 4, 5],
+  }),
+
+  computed: {},
+
+  methods: {},
+};
+</script>
