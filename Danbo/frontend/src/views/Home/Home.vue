48f8a900e822d96ba1e1932dc7dc35b8f6d071b6
pinerking <45725564+pinerking@users.noreply.github.com>
2020-10-25 20:30:51 +0800

    Revert "改了一下主页"
    This reverts commit 202928571ca1abcfb8bd170408746af600011b76.

--- Danbo/frontend/src/views/Home/Home.vue
+++ Danbo/frontend/src/views/Home/Home.vue
@@ -1,19 +1,9 @@
 <template>
   <v-app>
-    <v-card class="overflow-hidden" yellow lighten-5>
-      <TopBar />
-      <v-container fluid>
-        <v-row>
-          <v-col cols="2" sm="4" offset-sm="2">
-            <Tweets />
-          </v-col>
-
-          <v-col cols="2" sm="4">
-            <SideBar />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+      <v-card class="overflow-hidden" yellow lighten-5>
+        <TopBar />
+        <Tweets />
+      </v-card>
   </v-app>
 </template>
 
@@ -24,7 +14,6 @@ export default {
   components: {
     TopBar: () => import("../components/TopBar"),
     Tweets: () => import("../components/Tweets"),
-    SideBar: () => import("../components/SideBar"),
     // DashboardCoreDrawer: () => import('./components/Drawer'),
     // DashboardCoreSettings: () => import('./components/Settings'),
     // DashboardCoreView: () => import('./components/View'),
