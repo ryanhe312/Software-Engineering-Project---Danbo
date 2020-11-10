0089e8b6cb2e81b24e78eb717d72773ac67d174d
xyzforever <abothird19@gmail.com>
2020-11-10 19:34:20 +0800

    加入axios, 完成登录注册部分中与后端交互的逻辑

--- Danbo-frontend/src/views/components/SignUp.vue
+++ Danbo-frontend/src/views/components/SignUp.vue
@@ -3,48 +3,51 @@
     <v-row justify="center">
       <v-form ref="form">
         <v-text-field
-            label="Name"
-            :counter="20"
-            :rules="nameRules"
-            required
-            v-model="name"
+          label="Name"
+          :counter="20"
+          :rules="nameRules"
+          required
+          v-model="name"
         ></v-text-field>
         <v-text-field
-            label="E-mail"
-            :rules="emailRules"
-            required
-            v-model="email"
+          label="E-mail"
+          :rules="emailRules"
+          required
+          v-model="email"
         ></v-text-field>
         <v-text-field
-            label="Password"
-            type="password"
-            :rules="passwordRules"
-            required
-            v-model="password"
+          label="Password"
+          type="password"
+          :rules="passwordRules"
+          required
+          v-model="password"
         ></v-text-field>
         <v-text-field
-            label="Password Again"
-            type="password"
-            required
-            v-model="password_re"
+          label="Password Again"
+          type="password"
+          :rules="passwordRules"
+          required
+          v-model="password_re"
         ></v-text-field>
         <v-text-field
-            label="Verification Code"
-            :counter="4"
-            :rules="verificationRules"
-            required
-            v-model="verification_code"
+          label="Verification Code"
+          :counter="4"
+          :rules="verificationRules"
+          required
+          v-model="verification_code"
         ></v-text-field>
         <v-btn
-            color="success"
-            class="mr-12"
-            @click="req_verify"
-            id="btnVerify"
-            max-width="250"
-            min-width="200"
-            >Send Verification Code</v-btn
+          color="success"
+          class="mr-12"
+          @click="req_verify"
+          id="btnVerify"
+          max-width="250"
+          min-width="200"
+          >Send Verification Code</v-btn
         >
-        <v-btn color="warning" class="ml-12" @click="req_register">Sign Up</v-btn>
+        <v-btn color="warning" class="ml-12" @click="req_register"
+          >Sign Up</v-btn
+        >
       </v-form>
     </v-row>
   </v-container>
@@ -74,10 +77,117 @@ export default {
       (v) => !!v || "Verification code is required",
       (v) => (v && v.length == 4) || "Verification code must be 4 characters",
     ],
+    last_verify_time: 0,
   }),
+  mounted() {
+    this.checkLogin();
+  },
   methods: {
-    req_verify() {},
-    req_register() {},
+    req_register: function () {
+      if (
+        this.name == "" ||
+        this.password == "" ||
+        this.password_re == "" ||
+        this.email == "" ||
+        this.verification_code == ""
+      ) {
+        alert("请输入用户名、密码邮箱以及验证码！");
+      } else if (this.password != this.password_re) {
+        alert("两次输入的密码不一致！");
+      } else if (this.password.length < 6) {
+        alert("密码长度至少为6位！");
+      } else if (this.verification_code.length != 4) {
+        alert("验证码错误！");
+      } else {
+        this.axios
+          .post(
+            "/user/register",
+            (data = {
+              username: this.name,
+              password: this.password,
+              r_password: this.password_re,
+              email: this.email,
+              code: this.verification_code,
+            }),
+            {
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            }
+          )
+          .then((response) => this.ack_register(response))
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    },
+
+    ack_register: function (response) {
+      var data = response.data;
+      if (data.err_code == 200) {
+        alert("恭喜注册成功！请牢记用户名和密码");
+        this.$router.push("/");
+      } else {
+        alert("注册失败\n" + data.message);
+      }
+    },
+
+    req_verify: function () {
+      //验证邮箱格式
+      var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+      if (!myreg.test(this.email)) {
+        alert("邮箱格式错误");
+        return;
+      }
+      //验证发送间隔
+      var curtime = new Date().getTime();
+      if (curtime - this.last_verify_time < 60000) {
+        alert("发送过于频繁请一分钟后再试");
+        return;
+      }
+
+      this.last_verify_time = curtime;
+
+      this.axios
+        .post(
+          "/user/sendRegisterCode",
+          (data = {
+            email: this.email,
+          }),
+          {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          }
+        )
+        .then((response) => this.ack_verify(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+
+    ack_verify: function (response) {
+      var data = response.data;
+      if (data.err_code == 200) {
+        alert("发送验证码成功");
+      } else {
+        alert("发送验证码失败\n" + data.message);
+      }
+    },
+
+    async checkLogin() {
+      await axios
+        .post(url + "/user/getUsername/", (data = {}), {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        })
+        .then((response) => this.ack_ask_login_user(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+
+    ack_ask_login_user: function (response) {
+      if (response.data.err_code == 200) {
+        alert("已登录！");
+        this.$router.push("/");
+      }
+    },
   },
 };
 </script> 
