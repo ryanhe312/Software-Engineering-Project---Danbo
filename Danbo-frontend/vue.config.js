17ff6a4a85c359e8862ab8fdcf1e4c154b8558a6
xyzforever <abothird19@gmail.com>
2020-11-10 18:44:26 +0800

    设置代理

--- Danbo-frontend/vue.config.js
+++ Danbo-frontend/vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/user': {
+        target: "http://127.0.0.1:8000/",
+        changeOrigin: true,
+      },
+      '/blog': {
+        target: "http://127.0.0.1:8000/",
+        changeOrigin: true,
+      },
+    },
 }
