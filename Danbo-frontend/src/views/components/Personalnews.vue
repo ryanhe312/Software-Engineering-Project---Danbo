cdc01d79533044c14107b051cf45cfd1193a8d92
pinerking <45725564+pinerking@users.noreply.github.com>
2020-11-22 13:06:13 +0800

    个人信息交互逻辑

--- Danbo-frontend/src/views/components/Personalnews.vue
+++ Danbo-frontend/src/views/components/Personalnews.vue
@@ -1,52 +1,51 @@
 <template>
-  <v-card class="mx-n16" width="1000" height="800">
+  <v-card class="mx-n16" width="1100" height="800">
     <v-system-bar></v-system-bar>
     <v-card elevation="0" class="mt-4 mx-auto">
       <p class="title" align="center">Personal imformation</p>
     </v-card>
     <v-divider></v-divider>
-    <v-row class="mt-10">
-      <v-col cols="2" sm="4" offset-sm="0">
+    <v-row class="mt-14">
+      <v-col cols="2" sm="3" offset-sm="0">
         <v-card elevation="0" class="mt-2 d-flex justify-end mb-6">
           <p class="font-weight-regular" color="grey lighten-2">Nickname:</p>
         </v-card>
-        <v-card elevation="0" class="mt-6 d-flex justify-end mb-6">
+        <v-card elevation="0" class="mt-8 d-flex justify-end mb-6">
           <p class="font-weight-regular" color="grey lighten-2">E-mail:</p>
         </v-card>
-        <v-card elevation="0" class="mt-15 d-flex justify-end mb-6">
+        <v-card elevation="0" class="mt-16 d-flex justify-end mb-6">
           <p class="font-weight-regular" color="grey lighten-2">
             Personal signature:
           </p>
         </v-card>
-        <v-card elevation="0" class="mt-16 d-flex justify-end mb-6">
-          <p class="font-weight-regular" color="grey lighten-2">
-            Constellation:
-          </p>
+        <v-card elevation="0" class="mt-3 d-flex justify-end mb-6"> </v-card>
+        <v-card elevation="0" class="mt-15 d-flex justify-end mb-6">
+          <p class="font-weight-regular" color="grey lighten-2">Address:</p>
         </v-card>
         <v-card elevation="0" class="mt-7 d-flex justify-end mb-6">
           <p class="font-weight-regular" color="grey lighten-2">Gender:</p>
         </v-card>
-        <v-card elevation="0" class="mt-n1 d-flex justify-end mb-6">
+        <v-card elevation="0" class="mt-7 d-flex justify-end mb-6">
           <p class="font-weight-regular" color="grey lighten-2">Birthday:</p>
         </v-card>
       </v-col>
-      <v-col cols="2" sm="6" offset-sm="0">
+      <v-col cols="2" sm="7" offset-sm="0">
         <v-text-field solo dense v-model="name"></v-text-field>
         <v-text-field
-          class="mt-n1"
+          class="mt-1"
           v-text="mail"
           color="grey lighten-1"
         ></v-text-field>
         <v-textarea
-          class="mt-6"
+          class="mt-10"
           solo
           dense
           name="input"
           height="120"
-          v-model="input"
+          v-model="signature"
         ></v-textarea>
-        <v-select v-model="select" :items="cons" solo lense></v-select>
-        <v-radio-group class="mt-n1" v-model="radios" row>
+        <v-text-field solo dense v-model="address" class="mt-2"></v-text-field>
+        <v-radio-group class="mt-3" v-model="radios" row>
           <v-radio label="Male" value="Male"></v-radio>
           <v-radio label="Female" value="Female"></v-radio>
           <v-radio label="Secret" value="Secret"></v-radio>
@@ -61,7 +60,7 @@
         >
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
-              class="mt-n1"
+              class="mt-2"
               v-model="date"
               label="Birthday date"
               prepend-icon="mdi-calendar"
@@ -79,14 +78,39 @@
           ></v-date-picker>
         </v-menu>
       </v-col>
+      <v-col cols="2" sm="2" offset-sm="0">
+        <v-card elevation="0" class="mt-2">
+          <v-btn icon color="gray" @click="req_modifyName">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card>
+        <v-card elevation="0" class="mt-16" height="70"> </v-card>
+        <v-card elevation="0" class="mt-14">
+          <v-btn icon color="gray" @click="req_modifySignature">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card>
+        <v-card elevation="0" class="mt-9">
+          <v-btn icon color="gray" @click="req_modifyAddress">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card>
+        <v-card elevation="0" class="mt-8">
+          <v-btn icon color="gray" @click="req_modifyGender">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card>
+        <v-card elevation="0" class="mt-9">
+          <v-btn icon color="gray" @click="req_modifyBirthday">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card>
+      </v-col>
     </v-row>
     <div class="center">
-      <v-card class="mx-auto" elevation="0" width="666" height="10">
+      <v-card class="mx-16" elevation="0" width="666" height="10">
         <v-divider></v-divider>
       </v-card>
-    </div>
-    <div class="text-center">
-      <v-btn class="mt-14" rounded color="primary" dark> Save </v-btn>
     </div>
   </v-card>
 </template>
@@ -94,28 +118,24 @@
 <script>
 export default {
   data: () => ({
-    name: "Piner",
-    select: "Aquarius",
-    input: "my name is wh",
-    cons: [
-      "Aries",
-      "Taurus",
-      "Gemini",
-      "Cancer",
-      "Leo",
-      "Virgo",
-      "Libra",
-      "Scorpio",
-      "Sagittarius",
-      "Capricorn",
-      "Aquarius",
-      "Pisces",
-    ],
-    radios: "Male",
+    user: "",
+    name: "",
+    signature: "",
+    address: "",
+    radios: "",
     mail: "17307130181@fudan.edu.cn",
-    date: "1999-02-14",
+    date: "",
     menu: false,
   }),
+  created() {
+    this.req_user(),
+      this.req_name(),
+      this.req_signature(),
+      this.req_address(),
+      this.req_gender(),
+      this.req_birthday(),
+      this.req_email();
+  },
   watch: {
     menu(val) {
       val && setTimeout(() => (this.$refs.picker.activePicker = "YEAR"));
@@ -124,6 +144,261 @@ export default {
   methods: {
     save(date) {
       this.$refs.menu.save(date);
+    },
+    //获取用户名
+    req_user: function () {
+      this.axios
+        .post("/user/getUsername", {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_user(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_user: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.user = data.data;
+      } else {
+        alert(data.message);
+      }
+    },
+    //获取昵称
+    req_name: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getNickname", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_name(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_name: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.name = data.data;
+      } else {
+        this.name = data.data;
+      }
+    },
+    //修改昵称
+    req_modifyName: function () {
+      var formdata = new FormData();
+      formdata.append("name", this.name);
+      this.axios
+        .post("/user/modifyNickname", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_modifyName(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_modifyName: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        alert("昵称修改成功\n");
+        this.req_name();
+      } else {
+        alert(data.message);
+      }
+    },
+    //获取个性签名
+    req_signature: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getSignature", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_signature(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_signature: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.signature = data.data;
+      } else {
+        this.signature = data.data;
+      }
+    },
+    //修改个性签名
+    req_modifySignature: function () {
+      var formdata = new FormData();
+      formdata.append("signature", this.signature);
+      this.axios
+        .post("/user/modifySignature", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_modifySignature(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_modifySignature: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        alert("签名修改成功\n");
+        this.req_signature();
+      } else {
+        alert(data.message);
+      }
+    },
+    //获取地址
+    req_address: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getAddress", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_address(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_address: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.address = data.data;
+      } else {
+        this.address = data.data;
+      }
+    },
+    //修改地址
+    req_modifyAddress: function () {
+      var formdata = new FormData();
+      formdata.append("address", this.address);
+      this.axios
+        .post("/user/modifyAddress", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_modifyAddress(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_modifyAddress: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        alert("签名地址成功\n");
+        this.req_address();
+      } else {
+        alert(data.message);
+      }
+    },
+    //获取生日
+    req_birthday: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getBirthday", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_birthday(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_birthday: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.date = data.data;
+      } else {
+        this.date = data.data;
+      }
+    },
+    //修改生日
+    req_modifyBirthday: function () {
+      var formdata = new FormData();
+      formdata.append("birthday", this.date);
+      this.axios
+        .post("/user/modifyBirthday", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_modifyBirthday(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_modifyBirthday: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        alert("签名生日成功\n");
+        this.req_birthday();
+      } else {
+        alert(data.message);
+      }
+    },
+    //获取性别
+    req_gender: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getGender", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_gender(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_gender: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.radios = data.data;
+      } else {
+        this.radios = data.data;
+      }
+    },
+    //修改性别
+    req_modifyGender: function () {
+      var formdata = new FormData();
+      formdata.append("gender", this.radios);
+      this.axios
+        .post("/user/modifyGender", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_modifyGender(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_modifyGender: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        alert("性别修改成功\n");
+        this.req_gender();
+      } else {
+        alert(data.message);
+      }
+    },
+    //获取邮箱
+    req_email: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getEmail", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_email(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_email: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.email = data.data;
+      } else {
+        this.eamil = data.data;
+      }
     },
   },
 };
--- Danbo-frontend/src/views/components/Security.vue
+++ Danbo-frontend/src/views/components/Security.vue
@@ -6,90 +6,74 @@
     </v-card>
     <v-divider></v-divider>
     <v-row class="mt-10">
-      <v-card
-    class="mx-auto"
-    width="500"
+      <v-card class="mx-auto" width="500">
+        <v-card-title class="title font-weight-regular justify-space-between">
+          <span>{{ currentTitle }}</span>
+          <v-avatar
+            color="primary lighten-2"
+            class="subheading white--text"
+            size="24"
+            v-text="step"
+          ></v-avatar>
+        </v-card-title>
 
-  >
-    <v-card-title class="title font-weight-regular justify-space-between">
-      <span>{{ currentTitle }}</span>
-      <v-avatar
-        color="primary lighten-2"
-        class="subheading white--text"
-        size="24"
-        v-text="step"
-      ></v-avatar>
-    </v-card-title>
+        <v-window v-model="step">
+          <v-window-item :value="1">
+            <v-card-text>
+              <v-text-field label="Email" v-model="mail"></v-text-field>
+              <span class="caption grey--text text--darken-1">
+                This is the email you used to login to your Danbo account
+              </span>
+            </v-card-text>
+          </v-window-item>
 
-    <v-window v-model="step">
-      <v-window-item :value="1">
-        <v-card-text>
-          <v-text-field
-            label="Email"
-            v-model="mail"
-          ></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            This is the email you used to login to your Danbo account
-          </span>
-        </v-card-text>
-      </v-window-item>
+          <v-window-item :value="2">
+            <v-card-text>
+              <v-text-field
+                label="Verification Code"
+                v-model="codes"
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                type="password"
+                v-model="password"
+              ></v-text-field>
+              <v-text-field
+                label="Confirm Password"
+                type="password"
+                v-model="repassword"
+              ></v-text-field>
+              <span class="caption grey--text text--darken-1">
+                Please enter the vertification send to your mailbox, then change
+                your password
+              </span>
+            </v-card-text>
+          </v-window-item>
 
-      <v-window-item :value="2">
-        <v-card-text>
-            <v-text-field
-            label="Verification Code"
-            type="vertification"
-          ></v-text-field>
-          <v-text-field
-            label="Password"
-            type="password"
-          ></v-text-field>
-          <v-text-field
-            label="Confirm Password"
-            type="password"
-          ></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            Please enter the vertification send to your mailbox, then change your password
-          </span>
-        </v-card-text>
-      </v-window-item>
+          <v-window-item :value="3">
+            <div class="pa-4 text-center">
+              <v-img
+                class="mb-4"
+                contain
+                height="128"
+                src="https://cdn.vuetifyjs.com/images/logos/v.svg"
+              ></v-img>
+              <h3 class="title font-weight-light mb-2">Succeed!</h3>
+              <span class="caption grey--text">click to continue!</span>
+            </div>
+          </v-window-item>
+        </v-window>
 
-      <v-window-item :value="3">
-        <div class="pa-4 text-center">
-          <v-img
-            class="mb-4"
-            contain
-            height="128"
-            src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-          ></v-img>
-          <h3 class="title font-weight-light mb-2">
-            Succeed!
-          </h3>
-          <span class="caption grey--text">click to continue!</span>
-        </div>
-      </v-window-item>
-    </v-window>
+        <v-divider></v-divider>
 
-    <v-divider></v-divider>
-
-    <v-card-actions>
-      <v-btn
-        :disabled="step === 1"
-        text
-        @click="step--"
-      >
-        Back
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        color="primary"
-        depressed
-        @click="step++"
-      >
-        Continue
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <v-card-actions>
+          <v-btn :disabled="step === 1" text @click="step--"> Back </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" depressed @click="req_verify">
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-row>
   </v-card>
 </template>
@@ -97,17 +81,120 @@
 <script>
 export default {
   data: () => ({
-      step: 1,
-      mail:"17307130181@fudan.edu.cn",
+    step: 2,
+    email: "",
+    user: "",
+    codes: "",
+    password: "",
+    repassword: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
   }),
   computed: {
-      currentTitle () {
-        switch (this.step%3) {
-          case 1: return 'Verify mailbox'
-          case 2: return 'Change your password'
-          default: return 'Succeed'
-        }
-      },
+    currentTitle() {
+      switch (this.step % 3) {
+        case 1:
+          return "Verify mailbox";
+        case 2:
+          return "Change your password";
+        default:
+          return "Succeed";
+      }
     },
+  },
+  created() {
+    this.req_user(),
+    this.req_email();
+  },
+  methods: {
+    //获取用户名
+    req_user: function () {
+      this.axios
+        .post("/user/getUsername", {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_user(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_user: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.user = data.data;
+      } else {
+        alert("用户名不存在\n");
+      }
+    },
+    //获取邮箱
+    req_email: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      this.axios
+        .post("/user/getEmail", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_email(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_email: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.email = data.data;
+      } else {
+        this.eamil = data.data;
+      }
+    },
+    //发送验证码
+    req_verify: function () {
+      var formdata = new FormData();
+      formdata.append("email", this.email);
+      this.axios
+        .post("/user/sendLoginCode", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_verify(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_verify: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.step++;
+      } else {
+        alert("发送验证码失败\n");
+      }
+    },
+    //修改密码
+    req_modify: function () {
+      var formdata = new FormData();
+      formdata.append("user", this.user);
+      formdata.append("password", this.password);
+      formdata.append("r_password", this.repassword);
+      formdata.append("email", this.email);
+      formdata.append("code", this.codes);
+      this.axios
+        .post("/user/modifyPwd", formdata, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => this.ack_modify(response))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    ack_modify: function (response) {
+      var data = response.data;
+      if (data.error_code == 200) {
+        this.step++;
+      } else{
+        alert(data.message);
+      }
+    },
+  },
 };
 </script>
